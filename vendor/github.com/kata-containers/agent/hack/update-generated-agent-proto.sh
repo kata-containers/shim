c69802eabc516834947d3a604cdf7d4a14d62193
Hui Zhu <teawater@hyper.sh>
2017-12-08 11:49:34 +0800

    Update vendor
    Signed-off-by: Hui Zhu <teawater@hyper.sh>

---
+++ vendor/github.com/kata-containers/agent/hack/update-generated-agent-proto.sh
@@ -0,0 +1,6 @@
+#
+# Copyright 2017 HyperHQ Inc.
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+protoc -I $GOPATH/src/ --proto_path=protocols/grpc --gogo_out=plugins=grpc:protocols/grpc protocols/grpc/agent.proto protocols/grpc/oci.proto
